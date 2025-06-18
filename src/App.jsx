56be2e0409ba0d76695802b01cc461513d6c0932
LeandroMurillo <leandromurillo00@gmail.com>
2025-06-18 16:27:40 -0300

    se acomodó el layout de las paginas en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavbarComponent from './components/NavbarComponent';
 import SobreNosotros from './pages/SobreNosotros';
 import Home from './pages/Home';
 import NotFound from './pages/Error404';
-import Footer from './components/Footer';
 import RecomposicionCorporal from './pages/RecomposicionCorporal';
 import Musculacos from './pages/Musculacos';
+import Layout from './layouts/Layout';
 
 function App() {
   return (
     <Router>
-      <NavbarComponent />
-
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/nosotros" element={<SobreNosotros />} />
-        <Route path="/404" element={<NotFound />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/recomposicion" element={<RecomposicionCorporal />} />
-        <Route path="/musculacos" element={<Musculacos />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/nosotros" element={<SobreNosotros />} />
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/recomposicion" element={<RecomposicionCorporal />} />
+          <Route path="/musculacos" element={<Musculacos />} />
+        </Route>
       </Routes>
-      <Footer />
     </Router>
   );
 }
